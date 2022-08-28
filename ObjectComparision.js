fcbcfc8044b027e2667ba2dd3ad1d31784752716
Vijay Shankar <91515683+CodeVijay53@users.noreply.github.com>
2022-08-28 13:34:35 +0530

    Add files via upload

---
+++ ObjectComparision.js
@@ -0,0 +1,11 @@
+// To comapare two different objects
+var obj1 = { name: "Person 1", age: 5 };
+var obj2 = { age: 5, name: "Person 1" };
+let flag = true;
+for (var key in obj1) {
+  if (obj1[key] !== obj2[key]) {
+    flag = false;
+    break;
+  }
+}
+console.log(flag);
